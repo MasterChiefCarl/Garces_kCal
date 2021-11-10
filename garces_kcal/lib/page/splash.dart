af815eddb8d01d35b1fccf1b4204362f71ca30b3
Carl Benedict Garces <carlgarces7@gmail.com>
2021-11-10 12:41:32 +0800

    Splash Screen Initialized

---
+++ garces_kcal/lib/page/splash.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import '../page/home.dart';
+import '../main.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome()async{
+    await Future.delayed(Duration(seconds: 3),(){});
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MyHomePage(title: 'Hi There You Must see this Title',)));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Text(
+            'kCal',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold
+              
+            ),
+          ),
+        ),
+      ),
+    );
+
+
+
+  }
+}
---
+++ garces_kcal/lib/services/recipes.services.dart
@@ -0,0 +1,16 @@
+class Recipes {
+  int? rNumber; 
+  String? rName;
+  String? imageThumb;
+  String? rDescription;
+  List<String>? rIngredients;
+  List<String>? rProcess;
+
+
+  Recipes (String rName, String rDescription, List<String> rIngredients, List<String> rProcess){
+    this.rName = rName;
+    this.rDescription = rDescription;
+    this.rIngredients = rIngredients;
+    this.rProcess = rProcess;
+  }
+}
